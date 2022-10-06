1df18bfaf89e0671e2859a79bf1eee9908e38f5f
omargss <OMARELGRIBES07@gmail.com>
2022-10-06 16:55:20 +0200

    adding docs to repository / working on docs

--- app/modules/ConfigHandler.py
+++ app/modules/ConfigHandler.py
@@ -2,17 +2,21 @@ from os import path,environ
 from cerberus import Validator
 import yaml
 
-schema = {'VERSIONING': {'type': 'integer','min':0,'max':1},
+schema = {'VERSIONING': {'type': 'integer','min':0,'max':1}, 
         'EXPIRED_IN' : {'type': 'integer','min':0},
         }
 v = Validator(schema)
 
 
 class ConfigHandler:
+    '''Cette classe nous permet de manipuler la configuration en se basant sur les critères suivant :
+
+        - ***Historisation*** : activée ou pas.
+        - ***Délai d'expiration*** : nombre des jours. '''
     data = None
+    '''À priori, cette variable présente le **dictionnaire** qui constitue notre configuration. '''
     
     def __init__(self):
-        ''' this is the main '''
         if ConfigHandler.data != None:
             return
     
@@ -27,8 +31,10 @@ class ConfigHandler:
                 exit(1)
                 
     def get_versioning(self):
+        '''C'est le *getter* qui nous permet de récupèrer la valeur de *l'Historisation*.'''
         return self.data['VERSIONING']
     
     def get_expired_in(self):
+        '''C'est le *getter* qui nous permet de récupèrer la valeur du *délai d'expiration*.'''
         return self.data['EXPIRED_IN']
         
--- app/modules/DbHandler.py
+++ app/modules/DbHandler.py
@@ -2,12 +2,16 @@ import psycopg2
 
 
 class DbHandler:
+    '''Cette classe nous permet de manipuler la base de données 
+    qui reflète ce qui se passe au niveau du serveur distant. '''
     connection = None
+    '''C'est la variable qui nous permet d'intéragir avec notre base de données.'''
     
     def __init__(self, host, user, password, database):
         self.connection =  psycopg2.connect( host=host,database=database,user=user,password=password)
 
     def find_invalid_records(self):
+        '''Récupération d'un tuple qui contient les fichiers invalides'''
         sql = "SELECT * FROM v_archivage_files WHERE is_expired = {} OR is_valid={};".format(True,False)
         cur = self.connection.cursor()
         cur.execute(sql)
@@ -16,6 +20,7 @@ class DbHandler:
         return records 
 
     def update_all(self,checksum=""):
+        '''La mise à jour des fichiers contenus dans la base de données en fonction de la validité'''
         sql = "UPDATE archivage_files SET is_valid= {} WHERE checksum != '{}';".format(False,checksum)
         cur = self.connection.cursor()
         cur.execute(sql)
@@ -23,6 +28,7 @@ class DbHandler:
         cur.close()
 
     def find_all(self):
+        '''Sélection des fichiers qui ont dépassé le délai d'expiration'''
         sql = "SELECT * FROM v_archivage_files WHERE is_expired = {};".format(False)
         cur = self.connection.cursor()
         cur.execute(sql)
@@ -32,6 +38,7 @@ class DbHandler:
         
         
     def add_record(self,checksum,expiration_date,filename):
+        ''''''
         sql = """INSERT INTO archivage_files(checksum,expiration_date,filename) VALUES(%s,%s,%s)"""
         cur = self.connection.cursor()
         cur.execute(sql, (checksum,expiration_date ,filename))
