537a6ecd1c87590772f87e9e1a26a47a6b31cab0
RedRosh <filalirochdi8@gmail.com>
2022-10-13 21:49:19 +0200

    completing Server Web

--- app/modules/ServerWeb.py
+++ app/modules/ServerWeb.py
@@ -13,14 +13,19 @@ class ServerWeb :
         
     def retrieve_file(self,filename):
         '''Récupération du fichier à partir du serveur.'''
+        # We are using the requests library to retrieve the file from the server.
         response = requests.get('{}{}'.format(self.base_url,filename))
+        # we got different cases that we need to handle based on the response status code.
         match response.status_code:
             case 200:
+                # All good, we just to return the file.
                 logger.info("File retrieved successfully.")
                 return response.content
             case 404:
+                # The file doesn't exist.
                 raise Exception("File not found.")
             case _:
+                # Any other case.
                 raise Exception("An error occurred while retrieving the file.")
         
       
